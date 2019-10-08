861dbec957965f4b4235777de92651158f2130dc
oiizes <oiizesgitlab@gmail.com>
2019-10-08 20:13:05 +0300

    correcting factory to be dynamic

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    slug { "MyString" }
+    sequence(:title) { |n| "My awesome title #{n}" }
+    sequence(:content) { |n| "Read my long content #{n}" }
+    sequence(:slug) { |n| "Here is my amazing slug #{n}" }
   end
 end
