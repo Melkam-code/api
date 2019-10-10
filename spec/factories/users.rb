431850596517d319fd3d8d0210e26fa61143610e
oiizes <oiizesgitlab@gmail.com>
2019-10-10 18:36:02 +0300

    User validation, RSpec validation tests and correcting user factory to be dynamic.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    login { "MyString" }
-    name { "MyString" }
-    url { "MyString" }
-    avatar_url { "MyString" }
-    provider { "MyString" }
+    sequence(:login) { |n| "login number #{n}" }
+    sequence(:name) { |n| "my name number is #{n}" }
+    sequence(:url) { |n| "my url number is #{n}" }
+    sequence(:avatar_url) { |n| "avatar_url of #{n}" }
+    sequence(:provider) { |n| "provider number #{n}" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validation' do
+    it 'should have valid factory' do
+      user = build :user
+      expect(user).to be_valid
+    end
+
+    it 'should validate presence of attributes' do
+      user = build :user, login: nil, provider: nil
+      expect(user).not_to be_valid
+      expect(user.errors.messages[:login]).to include("can't be blank")
+      expect(user.errors.messages[:provider]).to include("can't be blank")
+    end
+
+    # it 'should validate uniqueness of login' do
+    #   user = create :user
+    #   user1 = create :user, login: user.login
+    #   expect(user1).not_to be_valid
+    #   user1.login = 'logmein'
+    #   expect(user1).to be_valid
+    # end
+  end
 end
